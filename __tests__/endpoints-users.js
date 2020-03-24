75344d64d579e85f3fd671386ffd2bc7b5e7ab46
Vardges-1 <vardgesbaghdasaryan93@hotmail.com>
2020-03-24 15:09:30 +0100

    zwei

--- __tests__/endpoints-users.js
+++ __tests__/endpoints-users.js
@@ -8,7 +8,13 @@ const faker = require('faker')
 let server;
 
 describe('Users Endpoints', () => {
-    /*test('should get list of all users', async done =>{
+    test('should get list of all users', async done =>{
+        await User.create({
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            password: faker.internet.password()
+        })
         const res = await request(app).get('/users')
         expect(res.statusCode).toBe(200)
         expect(Array.isArray(res.body)).toBeTruthy()
@@ -63,7 +69,7 @@ describe('Users Endpoints', () => {
             .send({email: fakeEmail})
         expect(res.body.email).toBe(fakeEmail)
         done()
-    })*/
+    })
 
     test('should create new user', async done => {
         const fakeUser = {
@@ -82,7 +88,6 @@ describe('Users Endpoints', () => {
 })
 
 beforeAll(async (done) => {
-    exec('npm run seed')
     server = app.listen(3000, () => {
         global.agent = request.agent(server);
         done();
